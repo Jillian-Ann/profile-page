9390efc52fac062d032228759f4931169c25a649
Jillian McLaren <jillian.mclaren@gmail.com>
2018-11-06 11:42:21 -0500

    working stepper with desc

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -4,7 +4,84 @@ import Stepper from "./Stepper";
 import ActionGTranslate from "material-ui/SvgIcon";
 import { Parallax } from "react-parallax";
 
+import artemis from "../images/fulls/artemis.jpg";
+import nutrivision from "../images/fulls/nutrivision.jpg";
+import bookhaven from "../images/fulls/bookhaven.jpg";
+
+const projects = [
+  {
+    label: "Artemis",
+    imgPath: artemis,
+    id: "1",
+    shortDesc: "Pandora for podcasts",
+    fullDesc:
+      "Artemis is a podcast recommendation and streaming single page application. A user can search for a genre to create channels in which they can rate played episodes. The channel learns what the user likes and curates future content based off the user's feedback. Artemis was built with JavaScript and the NERD stack - Node, Express, React-Redux, Postgres/Sequelize. The k-nearest neighbors algorithm was implemented to power a collaborative recommendation filtering system and the TF-IDF(Term Frequency-Inverse Document Frequency) algorithm and the Natural library were used to extract relevant tag names to feed to the recommendation engine. When a user likes or dislikes an episode, the tag scores associated with the channel are increased or decreased. Users are then played episodes from channels with similar tag scores to theirs. To ensure a consistent user experience, we created a clean and simple user interface with Material-UI.",
+    title: "Artemis"
+  },
+  {
+    label: "Nutri-Vision",
+    imgPath: nutrivision,
+    id: "5",
+    shortDesc: "Menu nutrtion analyzer",
+    fullDesc:
+      "iOS and Android app using JavaScript and React Native. Using Expo's Camera, the app takes an image and sends it to be analyzed by Google's Vision API. Google's OCR then reads the image and the returned text is filtered for menu items. The dishes are then sent to Spoonacular's API to retrieve nutrition information. An ordered list of dishes with their nutrition is ultimately displayed to the user.",
+    title: "Nutrition Vision"
+  },
+  {
+    label: "Book Haven",
+    imgPath: bookhaven,
+    id: "5",
+    shortDesc: "E-commerce platform for books",
+    fullDesc: "Photo 5",
+    title: "Book Haven"
+  }
+];
+
+// const styles = theme => ({
+//   header: {
+//     display: "flex",
+//     alignItems: "center",
+//     height: 50,
+//     paddingLeft: theme.spacing.unit * 4,
+//     backgroundColor: theme.palette.background.default
+//   },
+//   img: {
+//     width: "100%",
+//     flex: "1",
+//     // marginLeft: "48px",
+//     position: "relative",
+//     zIndex: "2",
+//     borderRadius: "1%"
+//   }
+// });
+
 class Projects extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentProject: 0
+    };
+    this.changeProject = this.changeProject.bind(this);
+    this.nextProject = this.nextProject.bind(this);
+    this.previousProject = this.previousProject.bind(this);
+  }
+
+  nextProject() {
+    this.setState(prevState => ({
+      currentProject: prevState.currentProject + 1
+    }));
+  }
+
+  previousProject() {
+    this.setState(prevState => ({
+      currentProject: prevState.currentProject - 1
+    }));
+  }
+
+  changeProject = currentProject => {
+    this.setState({ currentProject });
+  };
+
   render() {
     return (
       <div style={{ height: "100vh" }}>
@@ -13,11 +90,16 @@ class Projects extends React.Component {
             <h2 style={styles.title}>My Projects</h2>
             <section style={styles.section}>
               <div style={styles.projectDiv}>
-                <img src={fullArtemis} style={styles.projectPic} />
+                <Stepper
+                  changeProject={this.changeProject}
+                  previousProject={this.previousProject}
+                  nextProject={this.nextProject}
+                  projects={projects}
+                />
                 <div style={styles.description}>
-                  <h1>TITLE</h1>
-                  <h3>little descriptiom</h3>
-                  <Stepper style={styles.stepper} />
+                  <h1>{projects[this.state.currentProject].title}</h1>
+                  <h3>{projects[this.state.currentProject].shortDesc}</h3>
+                  <p>{projects[this.state.currentProject].fullDesc}</p>
                 </div>
                 <div style={styles.box} />
               </div>
@@ -37,13 +119,13 @@ const styles = {
     // height: "100vh"
   },
   div: {
-    right: "1%",
     position: "absolute",
-    padding: "0 32px",
-    maxWidth: "1184px",
-    margin: "0 auto"
+    padding: "0 32px"
+    // maxWidth: "1184px",
+    // margin: "0 auto"
   },
   title: {
+    left: "10%",
     textTransform: "uppercase",
     fontSize: "64px",
     lineHeight: "1em",
@@ -51,10 +133,10 @@ const styles = {
     zIndex: "3"
   },
   projectDiv: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "flex-start",
-    justifyContent: "flex-start"
+    display: "flex"
+    // flexDirection: "row",
+    // alignItems: "flex-start",
+    // justifyContent: "flex-start"
   },
   projectPic: {
     width: "50%",
@@ -65,10 +147,11 @@ const styles = {
     borderRadius: "1%"
   },
   description: {
+    overflow: "scroll",
     flex: "1",
     width: "431px",
     zIndex: "2",
-    padding: "10%"
+    padding: "0% 0% 0% 5%"
   },
   box: {
     height: "100%",
--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -2,80 +2,165 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import MobileStepper from "@material-ui/core/MobileStepper";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
 import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
 
-const styles = {
-  root: {
-    maxWidth: 400,
-    flexGrow: 1
+import artemis from "../images/fulls/artemis.jpg";
+import nutrivision from "../images/fulls/nutrivision.jpg";
+import bookhaven from "../images/fulls/bookhaven.jpg";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const tutorialSteps = [
+  {
+    label: "Artemis",
+    imgPath: artemis,
+    id: "1",
+    shortDesc: "Pandora for podcasts - podcast reccomendation app.",
+    fullDesc:
+      "Artemis is a podcast recommendation and streaming single page application. A user can search for a genre to create channels in which they can rate played episodes. The channel learns what the user likes and curates future content based off the user's feedback. Artemis was built with JavaScript and the NERD stack - Node, Express, React-Redux, Postgres/Sequelize. The k-nearest neighbors algorithm was implemented to power a collaborative recommendation filtering system and the TF-IDF(Term Frequency-Inverse Document Frequency) algorithm and the Natural library were used to extract relevant tag names to feed to the recommendation engine. When a user likes or dislikes an episode, the tag scores associated with the channel are increased or decreased. Users are then played episodes from channels with similar tag scores to theirs. To ensure a consistent user experience, we created a clean and simple user interface with Material-UI.",
+    title: "Artemis"
+  },
+  {
+    label: "Nutri-Vision",
+    imgPath: nutrivision,
+    id: "5",
+    shortDesc: "Photo 5",
+    fullDesc: "Photo 5",
+    title: "Photo 5"
+  },
+  {
+    label: "Book Haven",
+    imgPath: bookhaven,
+    id: "5",
+    shortDesc: "Photo 5",
+    fullDesc: "Photo 5",
+    title: "Photo 5"
   }
-};
+];
 
-class DotsMobileStepper extends React.Component {
+const styles = theme => ({
+  header: {
+    display: "flex",
+    alignItems: "center",
+    height: 50,
+    paddingLeft: theme.spacing.unit * 4,
+    backgroundColor: theme.palette.background.default
+  },
+  img: {
+    width: "100%",
+    flex: "1",
+    // marginLeft: "48px",
+    position: "relative",
+    zIndex: "2",
+    borderRadius: "1%"
+  }
+});
+
+class SwipeableTextMobileStepper extends React.Component {
   state = {
     activeStep: 0
   };
 
   handleNext = () => {
-    this.setState(state => ({
-      activeStep: state.activeStep + 1
+    this.props.nextProject();
+    this.setState(prevState => ({
+      activeStep: prevState.activeStep + 1
     }));
   };
 
   handleBack = () => {
-    this.setState(state => ({
-      activeStep: state.activeStep - 1
+    this.props.previousProject();
+    this.setState(prevState => ({
+      activeStep: prevState.activeStep - 1
     }));
   };
 
+  handleStepChange = activeStep => {
+    this.setState({ activeStep });
+    this.props.changeProject(this.state.activeStep);
+  };
+
   render() {
-    const { classes, theme } = this.props;
+    const {
+      classes,
+      theme,
+      projects,
+      nextProject,
+      previousProject,
+      changProject
+    } = this.props;
+    const { activeStep } = this.state;
+    const maxSteps = tutorialSteps.length;
 
     return (
-      <MobileStepper
-        variant="dots"
-        steps={6}
-        position="static"
-        activeStep={this.state.activeStep}
-        className={classes.root}
-        nextButton={
-          <Button
-            size="small"
-            onClick={this.handleNext}
-            disabled={this.state.activeStep === 5}
-          >
-            Next
-            {theme.direction === "rtl" ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button
-            size="small"
-            onClick={this.handleBack}
-            disabled={this.state.activeStep === 0}
-          >
-            {theme.direction === "rtl" ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          flex: "2",
+          marginLeft: "48px",
+          position: "relative",
+          zIndex: "2",
+          borderRadius: "1%"
+        }}
+      >
+        <Button
+          Button
+          size="small"
+          onClick={this.handleBack}
+          disabled={activeStep === 0}
+        >
+          {theme.direction === "rtl" ? (
+            <KeyboardArrowRight />
+          ) : (
+            <KeyboardArrowLeft />
+          )}
+        </Button>
+        <AutoPlaySwipeableViews
+          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+          index={activeStep}
+          onChangeIndex={this.handleStepChange}
+          enableMouseEvents
+        >
+          {projects.map((step, index) => (
+            <div key={step.label}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <img
+                  className={classes.img}
+                  src={step.imgPath}
+                  alt={step.label}
+                />
+              ) : null}
+            </div>
+          ))}
+        </AutoPlaySwipeableViews>
+        <Button
+          size="small"
+          onClick={this.handleNext}
+          disabled={activeStep === maxSteps - 1}
+        >
+          {theme.direction === "rtl" ? (
+            <KeyboardArrowLeft />
+          ) : (
+            <KeyboardArrowRight />
+          )}
+        </Button>
+      </div>
     );
   }
 }
 
-DotsMobileStepper.propTypes = {
+SwipeableTextMobileStepper.propTypes = {
   classes: PropTypes.object.isRequired,
   theme: PropTypes.object.isRequired
 };
 
-export default withStyles(styles, { withTheme: true })(DotsMobileStepper);
+export default withStyles(styles, { withTheme: true })(
+  SwipeableTextMobileStepper
+);
